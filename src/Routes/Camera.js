e62507095401c7d608957a9934aaace35f155b1a
Arek K <arekcommerce@gmail.com>
2022-12-13 13:48:57 +0100

    remove camera direction

--- src/Routes/Camera.js
+++ src/Routes/Camera.js
@@ -5,23 +5,12 @@ import App from '../App'
 const Camera = () => {
   const [isScan, setIsScan] = useState(true)
   const [data, setData] = useState()
-  const [cameraDirection, setCameraDricetion] = useState('user')
-
-  const onClickCameraDirection = () => {
-    cameraDirection === 'user'
-      ? setCameraDricetion('enviroment')
-      : setCameraDricetion('user')
-  }
 
   return (
     <App>
       <div style={{ width: '50vw', marginLeft: '100px' }}>
-        <button onClick={onClickCameraDirection}>
-          change camera direction
-        </button>
         {isScan && (
           <QrReader
-            facingMode={cameraDirection}
             onResult={(result, error) => {
               if (result && isScan) {
                 setData(result?.text)
