ae22773c58b2fcf473d9c9c5f853a5fb6ca5d5ef
Arek K <arekcommerce@gmail.com>
2022-12-23 13:11:47 +0100

    fix reachbility, fix deviceposition, fix icon

--- src/Routes/DevicePostion.js
+++ src/Routes/DevicePostion.js
@@ -2,60 +2,25 @@ import React, { useEffect, useState } from 'react'
 import App from '../App'
 
 const DevicePostion = () => {
-  const [coords, setCoords] = useState()
-  const [isLoading, setIsLoading] = useState(true)
+  const [coords, setCoords] = useState([])
 
   useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(({ coords }) => {
-        setCoords(coords)
-        setIsLoading(false)
-      })
+    const callaback = ({ alpha, beta, gamma }) => {
+      setCoords([alpha, beta, gamma])
     }
-  }, [coords?.speed])
 
-  if (isLoading) {
-    return <App>Loading....</App>
-  }
+    window.addEventListener('deviceorientation', callaback, true)
+
+    return () =>
+      window.removeEventListener('deviceorientation', callaback, true)
+  }, [])
+
   return (
     <App>
-      <h4>
-        If no value returned, it means that your device doesn't handle this
-        functionality
-      </h4>
-      <p>
-        <b>latitude</b>: Returns the position's latitude in decimal degrees:{' '}
-        <b>{coords?.latitude}</b>
-      </p>
-      <p>
-        <b>longitude:</b> Returns the position's longitude in decimal degrees:{' '}
-        <b>{coords?.longitude}</b>
-      </p>
-      <p>
-        <b>altitude:</b> Returns the position's altitude in meters, relative to
-        sea level: <b>{coords?.altitude}</b>
-      </p>
-      <p>
-        <b> accuracy:</b>Returns the accuracy of the latitude and longitude
-        properties in meters: <b>{coords?.accuracy}</b>
-      </p>
-      <p>
-        <b> altitudeAccuracy:</b>Returns the accuracy of the altitude property
-        in meters: <b>{coords?.altitudeAccuracy}</b>
-      </p>
-      <p>
-        <b> heading:</b> Returns the direction in which the device is traveling.
-        This value, specified in degrees, indicates how far off from heading
-        true north the device is. 0 degrees represents true north, and the
-        direction is determined clockwise (east is 90 degrees and west is 270
-        degrees). If speed is 0, heading is NaN. If the device is unable to
-        provide heading information, this value is null:{' '}
-        <b>{coords?.heading}</b>
-      </p>
-      <p>
-        <b> speed:</b>Returns the velocity of the device in meters per second.
-        This value can be null <b>{coords?.speed}</b>
-      </p>
+      <h2>position coords</h2>
+      <h3>alpha: {coords[0] ?? 0}</h3>
+      <h3>beta: {coords[1] ?? 0}</h3>
+      <h3>gamma: {coords[2] ?? 0}</h3>
     </App>
   )
 }
--- src/Routes/Reachability.js
+++ src/Routes/Reachability.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import App from '../App'
 
 const Reachability = () => {
-  const isOnLine = window.navigator.onLine
+  const [isOnline, setIsOnline] = useState(true)
+
+  useEffect(() => {
+    const online = () => setIsOnline(true)
+    const offline = () => setIsOnline(false)
+
+    window.addEventListener('online', online)
+    window.addEventListener('offline', offline)
+
+    return () => {
+      window.removeEventListener('online', online)
+      window.removeEventListener('offline', offline)
+    }
+  }, [])
+
   return (
     <App>
       <h1>Reachability</h1>
       <span>
-        you are <b>{isOnLine ? 'online' : 'offLine'}</b>
+        you are <b>{isOnline ? 'online' : 'offLine'}</b>
       </span>
     </App>
   )
